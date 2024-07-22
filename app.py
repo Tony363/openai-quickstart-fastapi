6b4cce6479c89ba8fdc1f262ca8be8893549817c
Tony363 <pysolver@gmail.com>
2024-07-22 18:02:48 -0400

    testing

--- app.py
+++ app.py
@@ -1,57 +1,123 @@
 import openai
-
+from openai import OpenAI
 import uvicorn
-
-from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse, RedirectResponse
+import base64
+import requests
+from fastapi import FastAPI, Request, Form,UploadFile,File
+from fastapi.responses import HTMLResponse, RedirectResponse,JSONResponse,RedirectResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
+import io
+import torch
+from PIL import Image
+from torchvision import transforms
+from torchmetrics.multimodal.clip_iqa import CLIPImageQualityAssessment
 
 
 class Settings(BaseSettings):
     OPENAI_API_KEY: str = 'OPENAI_API_KEY'
-
+    FLASK_APP: str = 'FLASK_APP'
+    FLASK_ENV: str = 'FLASK_ENV'
+    
     class Config:
         env_file = '.env'
 
 settings = Settings()
-openai.api_key = settings.OPENAI_API_KEY
-
+client = OpenAI(api_key=settings.OPENAI_API_KEY)
 
 app = FastAPI()
+transform = transforms.Compose([
+    transforms.ToTensor(),  # Converts the image to a PyTorch tensor
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Normalize for pre-trained models
+    #                     std=[0.229, 0.224, 0.225])
+])
+
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Function to encode the image
+def encode_image(image_path:str)->base64:
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 @app.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("upload_image.html", {"request": request})
 
 
-@app.post("/", response_class=HTMLResponse)
-def index(request: Request, animal: str= Form(...)):
-    response = openai.Completion.create(
-        model="text-davinci-002",
-        prompt=generate_prompt(animal),
-        temperature=0.6,
+@app.post("/get_json/{}", response_class=HTMLResponse)
+def index(request: Request,file_name: str):
+    # Getting the base64 string
+    base64_image = encode_image(file_name)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {settings.OPENAI_API_KEY}"
+    }
+
+    payload = {
+        "model": "gpt-4o-mini",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                    "type": "text",
+                    "text": "What’s in this image?"
+                },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/jpeg;base64,{base64_image}"
+                        }
+                }
+            ]
+        }
+    ],
+    "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+
+    print(response.json())
+    return response.json()
+    # return templates.TemplateResponse("json_out.html", {"request": request, "data": response.json()})
+
+@app.post("/output_iqa")
+async def create_upload_file(request:Request,file: UploadFile = File(...)):
+    # Read the image file into a PIL Image
+    print(file)
+    image_content = await file.read()
+    image = Image.open(io.BytesIO(image_content)).convert('RGB')
+    img = transform(image).unsqueeze(0)
+    metric = CLIPImageQualityAssessment(
+        model_name_or_path='openai/clip-vit-base-patch16',
+        prompts=(("Good Photo.", "Bad Photo."), "quality")
     )
-    result = response.choices[0].text
-    return templates.TemplateResponse("index.html", {"request": request, "result": result})
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+    out = {
+        'on':"quality",
+        "prompts":("Good Photo.", "Bad Photo."),
+        "score": metric(img)['quality'],
+    }
+    if out['score'] > 0.7:
+        return RedirectResponse(url=f'/get_json/?filename={file.filename}')
+    
+    return templates.TemplateResponse(
+        "json_out.html", 
+        {"request": request, "data": out}
     )
+
+    # Respond with the random float and image info
+    return JSONResponse(content={"IQA": metric(img),'text':'wtf'})
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     uvicorn.run('app:app', host="localhost", port=5001, reload=True)
---
+++ test.py
@@ -0,0 +1,32 @@
+# https://lightning.ai/docs/torchmetrics/stable/multimodal/clip_iqa.html
+import torch
+from torchmetrics.multimodal.clip_iqa import CLIPImageQualityAssessment
+from torchvision import transforms
+from PIL import Image
+
+
+def load_image(
+    image_path:str,
+    transforms:transforms.Compose
+)->torch.tensor:
+    # Open the image file
+    img = Image.open(image_path).convert('RGB')  # Convert to RGB
+    img_tensor = transform(img)  # Apply the transformation
+    return img_tensor
+
+if __name__ == '__main__':
+    path = 'Complex Imaging Tags _ Metal Marker Mfg (1).jpeg'
+    _ = torch.manual_seed(42)
+    # Define a transform to convert the image to a tensor and normalize it
+    transform = transforms.Compose([
+        transforms.ToTensor(),  # Converts the image to a PyTorch tensor
+        # transforms.Normalize(mean=[0.485, 0.456, 0.406],  # Normalize for pre-trained models
+        #                     std=[0.229, 0.224, 0.225])
+    ])
+    img = load_image(path,transform).unsqueeze(0).float()
+    print(img.shape)
+    metric = CLIPImageQualityAssessment(
+        model_name_or_path='openai/clip-vit-base-patch16',
+        prompts=(("Good Photo.", "Bad Photo."), "quality")
+    )
+    print(metric(img))
